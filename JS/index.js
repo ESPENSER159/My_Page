07f21ba1b90a05ddc237a9359328eac656f44ab8
Steven <stevencaro30@gmail.com>
2018-10-17 13:53:34 -0500

    cambios

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,9 @@
 
+// Detectar si es un dispositivo movil
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+
+}
+
 function btnini(){
 	document.getElementById("frameload").src 
 	= "htmls/ini.html";
