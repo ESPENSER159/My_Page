3ec82a472ff484deab1da7224bd5f13b692e7bb8
Steven <stevencaro30@gmail.com>
2018-10-18 00:08:06 -0500

    cambio de apk y vista para dispositivos moviles

--- Projects/fizzbuzz/fizzbuzz.js
+++ Projects/fizzbuzz/fizzbuzz.js
@@ -12,11 +12,7 @@ input.addEventListener("keyup", function(event) {
   event.preventDefault();
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
-
-  	clearDiv();
-  	numeros = input.value;
-  	mostrar();
-
+  	botonOk();
   }
 });
 
@@ -29,19 +25,18 @@ function clearDiv(){
 }
 
 function mostrar(){
-	clearDiv();
 	
 	for(var i = 1; i <= numeros; i++)
 	{
 		if(i % 3 == 0)
 		{
-			var p = "<p><span style='color: tomato'>Fizz</span><span style='color:#212121; font-size: 15px;'>("+i+")</span></p>";
+			var p = "<p><span style='color: tomato'>Fizz</span><span style='color:#212121;'>("+i+")</span></p>";
 			document.getElementById("guide").insertAdjacentHTML('beforebegin', p);
 		}
 
 		if(i % 5 == 0)
 		{
-			var p = "<p><span style='color: orange'>Buzz</span><span style='color:#212121; font-size: 15px;'>("+i+")</span></p>";
+			var p = "<p><span style='color: orange'>Buzz</span><span style='color:#212121;'>("+i+")</span></p>";
 			document.getElementById("guide").insertAdjacentHTML('beforebegin', p);
 		}
 
@@ -53,5 +48,67 @@ function mostrar(){
 	}
 	
 }
+
+function botonOk(){
+	clearDiv();
+  	numeros = input.value;
+  	mostrar();
+}
+
+
+// Detectar si es un dispositivo movil
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+	 // Titulo
+	 var title = document.getElementById("title");
+	 title.style.width = "50vw";
+	 title.style.marginLeft = "0px";
+
+	// id espacio
+	for(var t = 0; t <= 7; t++){
+		document.getElementById("espacio"+t).setAttribute("style", "width: 12vw; padding: 0; float:left; height:auto;");
+	}
+
+	// id letters
+	for (var i = 0; i <= 7; i++) {
+		var divsLetters = document.getElementById(i);
+		divsLetters.setAttribute("id","letters"+i);
+	}
+	for (var j = 0; j <= 7; j++) {
+		document.getElementById("letters"+j).setAttribute("style", "font-size: 12vw; text-align: center; color: #00c3ff; font-weight: bold;");
+	}
+
+	// perdir cantida
+	document.getElementById("cantidad").setAttribute("style", "width: 25%; height: 4vh; font-size: 4vw; margin-top: 50px;");
+
+	// Boton Ok
+	document.getElementById("botonOk").setAttribute("style", "font-size: 4vw;");
+
+	// Div de respuestas
+	document.getElementById("content").setAttribute("style", "width: 50%; height: 70%; font-size: 5vw;");
+
+	// Texto
+	let clock = setInterval(() => {
+        clearInterval(clock)
+        clock = null
+        document.getElementById('textFB').style.fontSize = '5vw'
+    }, 1500)
+
+}
+else {
+	// id espacio
+	for(var t = 0; t <= 7; t++){
+		document.getElementById("espacio"+t).setAttribute("style", "padding: 0; width: 5vw; float:left; height:auto;");
+	}
+
+	// id letters
+	for (var i = 0; i <= 7; i++) {
+		var divsLetters = document.getElementById(i);
+		divsLetters.setAttribute("id","letters"+i);
+	}
+	for (var j = 0; j <= 7; j++) {
+		document.getElementById("letters"+j).setAttribute("style", "text-align: center; font-size: 5vw; color: #00c3ff; font-weight: bold;");
+	}
+}
+
 
 
